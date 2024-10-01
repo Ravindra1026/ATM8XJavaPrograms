7a838c93f59d4f9c1234895d96d68307d8cca3b7
Ravindra Singh <rathoreravindra1026@gmail.com>
2024-10-01 08:15:49 +0530

    Practice class updated

---
+++ src/Practice/ForLoop/PrintFirst10No.java
@@ -0,0 +1,11 @@
+package Practice.ForLoop;
+
+public class PrintFirst10No {
+    public static void main(String[] args) {
+        for(int i =1; i<=10;i++){
+            System.out.println(i);
+        }
+    }
+    //For loop
+
+}
